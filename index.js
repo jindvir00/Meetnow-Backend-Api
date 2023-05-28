b0d792e741eca7c83d4c60a0cc31af8d84b58dfd
JINDVIR SINGH <68278240+jindvir@users.noreply.github.com>
2023-05-28 13:24:26 +0530

    route update

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.post('/register' , async(req, res) => {
     }
 })
 
-app.get( "/login/:email" , async(req, res) => {
+app.get( "/login" , async(req, res) => {
     // res.send( "Hello I am live")
 
     try {
